489f305d47a9507b1bd013acce7fbe9e5a97b4d3
Nicolas Godinho <nicolasdsgodinho@gmail.com>
2024-11-09 12:46:11 -0300

    responsivo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
-  $('#mobile_btn').on('click', function () {
-      $('#mobile_menu').toggleClass('active');
-      $('#mobile_btn').find('i').toggleClass('fa-x');
+document.addEventListener("DOMContentLoaded", function() {
+  const mobileBtn = document.getElementById('mobile_btn');
+  const mobileMenu = document.getElementById('mobile_menu');
+  const header = document.getElementById('header');
+  const icon = mobileBtn.querySelector('i');
+
+  mobileBtn.addEventListener('click', function() {
+    mobileMenu.classList.toggle('active');
+    header.classList.toggle('active');
+    icon.classList.toggle('bi-list');
+    icon.classList.toggle('bi-x-lg'); // Alterna entre ícones
   });
 });
